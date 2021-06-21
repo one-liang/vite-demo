6606d587f66aca27c0a6392aabbbdc7e6029de79
william <william810212@gmail.com>
2021-06-22 00:25:23 +0800

    feat: add animation by gsap

--- src/components/AxiosDemo.vue
+++ src/components/AxiosDemo.vue
@@ -1,11 +1,20 @@
 <template>
-  <ul class="mx-auto max-w-prose">
-    <li v-for="item in data" :key="item.sno">{{ item.sno }} | {{ item.sarea }} | {{ item.sna }}</li>
-  </ul>
+  <transition-group
+    appear=""
+    tag="ul"
+    class="mx-auto max-w-prose"
+    @before-enter="beforeEnter"
+    @enter="enter"
+  >
+    <li v-for="(item, index) in data" :key="item.sno" :data-index="index">
+      {{ item.sno }} | {{ item.sarea }} | {{ item.sna }}
+    </li>
+  </transition-group>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import gsap from "gsap";
 
 export default {
   name: "AxiosDemo",
@@ -16,12 +25,27 @@ export default {
   },
   mounted() {
     axios
-      .get(
-        "api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json/preview"
-      )
+      .get("api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json/preview")
       .then((res) => {
-        this.data = res.data
+        this.data = res.data;
       });
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter");
+      el.style.opacity = 0;
+      el.style.transfrom = "translateY(100px)";
+    },
+    enter(el, done) {
+      console.log("enter");
+      gsap.to(el, {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        onComplete: done,
+        delay: el.dataset.index * 0.03,
+      });
+    },
   },
 };
 </script>
